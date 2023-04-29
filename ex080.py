a8f0bbd907b9658dd1d245f952f8fb94cb0cf330
Gabriel Felix <124117592+Gaabriel-F@users.noreply.github.com>
2023-04-29 20:08:45 -0300

    Update ex080.py

--- ex080.py
+++ ex080.py
@@ -1,31 +1,18 @@
 NumbersList = []
-
 for Loop in range(5):
-
     NumbersFromInput = int(input('Type a number: '))
-
     if Loop == 0 or NumbersFromInput > NumbersList[-1]:
-
         NumbersList.append(NumbersFromInput)
-
         print('value added at the last position of the list')
-
+        
     else:
-
         Position = 0
-
         while Position < len(NumbersList):
-
             if NumbersFromInput <= NumbersList[Position]:
-
                 NumbersList.insert(Position, NumbersFromInput)
-
                 print(f'Value added at the {Position} position of the list')
-
                 break
-
             Position += 1
 
 print('\n' + '-=' * 40)
-
 print(f'The sequence of values entered was {NumbersList}')
