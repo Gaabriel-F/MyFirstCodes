80a74688b27a5b3f2643112754ecd0ec5da899a5
Gabriel Felix <124117592+Gaabriel-F@users.noreply.github.com>
2023-04-29 12:03:15 -0300

    Restructuration 008.py

--- ex008.py
+++ ex008.py
@@ -1,32 +1,8 @@
-#ex005
-
-num = int(input("Digite um numero: "))
-print()
-rst0 = num-1
-rst1 = num+1
-
-db = num*2
-db2 = num*3
-db22 = num**(1/2)
-
-print(f'O número escolhido foi {num}! O seu antecessor é {rst0}, e o seu sucessor é {rst1} ')
-print()
-print(f' Agora mostrarei o dobro, triplo e a raiz do número {num}! \n O dobro é {db}, já o triplo é esse aí que ta na telinha {db2}, e a raiz é essa aqui minha shawty {db22:.2f} ')
-
-print ()
-
-Al = str(input('Digite o nome do aluno: '))
-mA= str(input('Insira a materia em que o aluno tirou as notas a seguir: '))
-b1 = int(input('nota no primeiro bimestre: '))
-b2 = int(input('nota segundo bimestre: '))
-b3 = int(input('nota terceiro bimestre: '))
-b4 = int(input('nota quarto bimestre: '))
-
-me = b1+b2+b3+b4
-
-rT = me//4
-
-print (f'a média do(a) {Al} em {mA} foi {rT}!')
-
-#ex008 - transformando medidas
-
+Distancia = float(input('Digite uma distância em metros: '))
+print(f'''A medida de {Distancia:.2f}m corresponde a
+{Distancia*10} dm
+{Distancia*100} cm
+{Distancia*1000} mm
+{Distancia/10} dam
+{Distancia/100} hm
+{Distancia/1000} Km''')
