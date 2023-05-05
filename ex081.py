436105946a9c19c06cdbc106eb5e83a4c62e9666
Gabriel Felix <124117592+Gaabriel-F@users.noreply.github.com>
2023-05-05 13:37:50 -0300

    second commit
    Adding comments to the code, changing the if condition to the ternary operator, and removing the "CounterNumbers" variable responsible for counting.

--- ex081.py
+++ ex081.py
@@ -1,9 +1,13 @@
+'''This code will run by asking some values
+to the user and in the final of the code will
+appear the results of: Value 5 is in the list?
+List in order, quantity of elements typed.'''
+
 NumbersList = []
-CounterNumbers = 0
+
 while True:
     ValuesInput = int(input('Type a value: '))
     NumbersList.append(ValuesInput)
-    CounterNumbers += 1
 
     # Ask to user if he wants to continue typing numbers.
     Continuation = input('Do you want to continue? Y/N: ').strip().lower()[0]
@@ -13,11 +17,9 @@ while True:
         continue
     if Continuation == 'n':
         break
+
 # Final results.
 print('-='*40)
-print(f'''You typed {CounterNumbers} elements.
+print(f'''You typed {len(NumbersList)} elements.
 The descending order of the list is {sorted(NumbersList, reverse=True)}''')
-if 5 in NumbersList:
-    print('The value 5 is in the list')
-else:
-    print('the value 5 doesn`t appear in the list')
+print('The value 5 is in the list' if 5 in NumbersList else 'the value 5 doesn`t appear in the list')
