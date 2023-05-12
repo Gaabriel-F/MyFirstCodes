59a883df9bfe3f4440ffb3290da78597d9a05d98
Gabriel Felix <124117592+Gaabriel-F@users.noreply.github.com>
2023-05-12 10:42:30 -0300

    second commit
    I'm adding a key named 'Situation' to the dictionary and now displaying not only the student's status but also their name, average, and status to the console.

--- ex090.py
+++ ex090.py
@@ -4,7 +4,7 @@
 studentGrade = {}
 
 # Adding keys along with values to the named dictionary 'studentGrade'.
-studentGrade['Name'] = str(input("Name: "))
+studentGrade['Name'] = str(input("Name: ")).strip().title()
 studentGrade['Average'] = float(input(f"What was {studentGrade['Name']}`s avarage?: "))
 
 # Final part.
@@ -12,10 +12,15 @@ print('-='*25)
 
 # Verify if the student is aproved, in recovery or reproved.
 if studentGrade['Average'] >= 7.0:
-    print('STATUS: \033[1;32mAPROVED\033[0;0m')
+    studentGrade['Situation'] = "\033[1;32mAPROVED\033[0;0m"
 
 elif studentGrade['Average'] >= 5.0 and studentGrade['Average'] < 7.0:
-    print('STATUS: \033[1;33mIN RECOVERY\033[0;0m')
+    studentGrade['Situation'] = "\033[1;33mIN RECOVERY\033[0;0m"
 
 else:
-    print('STATUS: \033[1;31mREPROVED\033[0;0m')
+    studentGrade['Situation'] = "\033[1;31mREPROVED\033[0;0m"
+
+# Displaying the values of the keys in a dictionary 
+print(f'''STUDENT NAME: {studentGrade["Name"]}
+AVARAGE: {studentGrade["Average"]}
+STATUS: {studentGrade['Situation']}''')
