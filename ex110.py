14ba1f29de5b61e6b97b2fc5c18feea2dfb9a862
Gabriel Felix <124117592+Gaabriel-F@users.noreply.github.com>
2024-01-03 21:43:53 -0300

    first commit
    ex110 and his module

---
+++ ex110.py
@@ -0,0 +1,4 @@
+from utils import functions
+
+price = int(input("Type a price: R$"))
+functions.Summary(price, 5, 9)
--- functions.py
+++ functions.py
@@ -1,18 +1,19 @@
-def Half(value):
-    return value / 2
+def Summary(price = 0, adding = 0, reducing = 0):
+    print('-'*30)
+    print(f'{"ANALIZING THE VALUE":^30}')
+    print('-'*30)
 
+    Double = price * 2
+    Half = price / 2
+    
+    percent1 = (price * adding) / 100
+    resultAdding = price + percent1
 
-def Double(value):
-    return value * 2
+    percent2 = (price * adding) / 100
+    resultReducing = price - percent2
 
-
-def ReduceByPercent(value, percent):
-    percent1 = (value * percent) / 100
-    Reducing = value - percent1    
-    return Reducing
-
-
-def AddByPercent(value, percent):
-    percent1 = (value * percent) / 100
-    Adding = value - percent1    
-    return Adding
+    print(f'Analized price: \t{price:.2f}')
+    print(f'Twice the price: \t{Double:.2f}')
+    print(f'Half the price: \t{Half:.2f}')
+    print(f'adding {adding}%: \t\t{resultAdding:.2f}\t')
+    print(f'reducing {reducing}%: \t\t{resultReducing:.2f}')
